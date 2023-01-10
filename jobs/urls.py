1a8568a48b018c6c747fcdb9ee95de35929c7209
ssakthi <ssakthi@iu.edu>
2023-01-10 18:10:58 -0500

    Base frontend work

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import index, CollaboratorListView, CollaboratorDetailView
 
 urlpatterns = [
-    path("", CollaboratorListView.as_view(), name='home'),
+    path("", CollaboratorListView.as_view(), name='collaborator_list'),
     path("developer/<int:pk>/", CollaboratorDetailView.as_view(), name = 'collaborator-detail'),
 ]
