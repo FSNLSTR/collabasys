729023613dfec1f3cf6d1f29b4628d2cb246c3e8
ygollapudi <89621405+ygollapudi@users.noreply.github.com>
2023-01-23 23:43:40 -0500

    Added file paths to support signin/signup

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from . import views
 from .views import index, CollaboratorListView, CollaboratorDetailView
 
 urlpatterns = [
     path("", CollaboratorListView.as_view(), name='collaborator_list'),
     path("developer/<int:pk>/", CollaboratorDetailView.as_view(), name = 'collaborator-detail'),
+    path("register", views.register_request, name = "register"),
+    path("logout", views.logout_request, name = "logout"),
+    path("login", views.login_request, name = "login"),
 ]
